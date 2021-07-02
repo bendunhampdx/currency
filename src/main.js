f53ae3e72b6e6a0a11c41b8b17e6487cec21b676
bendunhampdx <bendunhampdx@gmail.com>
2021-07-02 09:18:04 -0700

    update index form to include currency to convert to/main JS function

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyConverter from './services/js/currencyApi'
+
+function displayCurrencyConverter(response){
+  if (response) {
+    $('.currencyconverter').empty();
+    for (let i =0; i < response.conversion_rates.length; i++) {
+  $('.currencyconverter').append(`
+  <tr>
+    <td>${response.conversion_rates[i]}</td>
+  </tr>`);
+  }
+  } else 
+  {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
 
 
 $(document).ready(function() {
