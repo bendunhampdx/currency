c21547ccc7acb2296717d95f20f57ffceae03d6d
bendunhampdx <bendunhampdx@gmail.com>
2021-07-02 15:04:03 -0700

    WIP

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyConverter from './services/js/currencyApi.js'
 
-function getNewCurrency(response) {
+function getElements(response) {
   let country = $('#country').val();
   let AEDConversion = $('#dollar').val() * response.conversion_rates.AED
   let AFNConversion = $('#dollar').val() * response.conversion_rates.AFN
   let ALLConversion = $('#dollar').val() * response.conversion_rates.ALL
   let AMDConversion = $('#dollar').val() * response.conversion_rates.AMD
   let ANGConversion = $('#dollar').val() * response.conversion_rates.ANG
+  let PUDConversion = $('#dollar').val() * response.conversion_rates.PUD
   if (response) {
     if(country === 'United Emirates') {
       $('#currency').html(`The conversion in AMD is ${AEDConversion}`)
@@ -27,6 +28,9 @@ function getNewCurrency(response) {
     else if(country === 'Netherlands') {
       $('#currency').html(`The conversion in ANG is ${ANGConversion}`)
     }
+    else if(country === 'Puddleland') {
+      $('#currency').html(`The conversion in PUD is ${PUDConversion}`)
+    }
   } else {
     $('.showErrors').text(`There was an error: ${response.message}`);
   }
@@ -37,7 +41,7 @@ $(document).ready(function() {
   $('#currency').click(function() {
     CurrencyConverter.getNewCurrency()
     .then(function(response) {
-      getNewCurrency(response)
+      getElements(response)
     })
   })
   });
